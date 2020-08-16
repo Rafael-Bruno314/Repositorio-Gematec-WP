8cb70461c1cf9a94aa9b934135083946ee2073fa
Rafael-Bruno314 <rafaelbruno.cunha@gmail.com>
2020-08-16 02:55:38 -0300

    Weekly backup - August 16, 2020

--- wp-config.php
+++ wp-config.php
@@ -20,6 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
+define('REVISR_GIT_PATH', ''); // Added by Revisr
 define( 'DB_NAME', 'gematec-wp' );
 
 /** MySQL database username */
